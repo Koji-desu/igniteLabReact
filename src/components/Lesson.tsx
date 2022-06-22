1c76a4c6b6b644ed03c8606fa3146c4f70804288
Koji-Desu <pdigo153@gmail.com>
2022-06-22 03:30:42 -0300

    aula2

---
+++ src/components/Lesson.tsx
@@ -0,0 +1,48 @@
+import { CheckCircle, Lock } from 'phosphor-react'
+import { isPast} from 'date-fns'
+
+interface  LessonProps  {
+    title: string;
+    slug: string;
+    availableAt: Date;
+    type: 'live' | 'class';
+}
+
+export function Lesson (props: LessonProps){
+    const isLessonAvailable = false;
+    return(
+        <a href="">
+            <span className="text-gray-300"> 
+                {props.availableAt.toString()}
+
+            </span>
+
+            <div className="rounded border border-gray-500 p-4 mt-2">
+                <header className="flex items-center justify-between">
+                    {props.availableAt? (
+                        <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+                        <CheckCircle size={20} />
+                        Contéudo liberado
+
+                    </span>
+                    ): (
+                        <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+                        <Lock size={20} />
+                         Em Breve
+                    </span>
+                )}
+
+                    <span className="text-xs rounded px-2 py-[2px] text-white border border-green-300 font-bold">
+                        {props.type === 'live' ? 'AO VIVO' : 'AULAA PRATICA'}
+
+                    </span>
+                </header>
+
+                <strong className="text-gray-200 mt-5 block">
+                    {props.title}
+
+                </strong>
+            </div>
+        </a>
+    )
+}
